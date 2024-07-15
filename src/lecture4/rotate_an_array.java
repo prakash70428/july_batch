8fb18278a8227028f97507f518749f2df6c69304
prakash70428 <prakashmendish@gmail.com>
2024-07-15 22:21:37 +0530

    lecture 4 updated

--- src/lecture4/rotate_an_array.java
+++ src/lecture4/rotate_an_array.java
@@ -1,21 +1,32 @@
 package lecture4;
 import java.util.*;
-public class reverse_of_array {
+public class rotate_an_array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for(int i=0 ;i < n;i++) {
+        for(int i=0 ; i < n;i++) {
         	arr[i] = scn.nextInt();
         }
         
-        reverse(arr,0,arr.length - 1);
-        display(arr);
+        int k = scn.nextInt();
+        rotate(arr,k);
 	}
-	public static void reverse(int[] arr,int left,int right) {
+	
+	public static void rotate(int[] arr,int k) {
+		k = k % arr.length;
+		if(k < 0) {
+			k = k + arr.length;
+		}
 		
+		reverse(arr,0,arr.length - 1);
+		reverse(arr,0,k - 1);
+		reverse(arr,k,arr.length - 1);
+	}
+	
+    public static void reverse(int[] arr,int left,int right) {
 		while(left < right) {
 			int temp = arr[left];
 			arr[left] = arr[right];
@@ -24,12 +35,6 @@ public class reverse_of_array {
 			left++;
 			right--;
 		}
-	}
-	public static void display(int[] arr) {
-		int n = arr.length;
-		for(int i=0 ;i < n;i++) {
-        	System.out.println(arr[i]);
-        }
 	}
 
 }
