077ee56b4b47fc1802becabcbf9fac3b7899f156
prakash70428 <prakashmendish@gmail.com>
2024-07-20 12:57:27 +0530

    lecture 4 updated

---
+++ src/lecture4/sort01.java
@@ -0,0 +1,42 @@
+package lecture4;
+import java.util.*;
+public class sort01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0 ; i < n;i++) {
+        	arr[i] = scn.nextInt();
+        }
+        
+        sort01(arr);
+        display(arr);
+	}
+	public static void sort01(int[] arr) {
+		int i = 0;
+		int j = 0;
+		
+		while(i < arr.length) {
+			if(arr[i] == 0) {
+				swap(arr,i,j);
+				i++;
+				j++;
+			}else {
+				i++;
+			}
+		}
+	}
+	public static void swap(int[] arr,int i,int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	public static void display(int[] arr) {
+		for(int i=0 ;i < arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
