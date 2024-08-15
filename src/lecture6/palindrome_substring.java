5f7471a0ecb9180dc11fa1ac57222e685350f416
prakash70428 <prakashmendish@gmail.com>
2024-08-15 14:11:42 +0530

    lecture 6 updated

---
+++ src/lecture6/palindrome_substring.java
@@ -0,0 +1,36 @@
+package lecture6;
+import java.util.*;
+public class palindrome_substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        palindromesubstring(str);
+	}
+	public static void palindromesubstring(String str) {
+		for(int si=0;si < str.length();si++) {
+			for(int ei=si ;ei < str.length();ei++) {
+				if(isPalindrome(str.substring(si,ei + 1)) == true) {
+					System.out.println(str.substring(si,ei + 1));
+				}
+			}
+		}
+	}
+	public static boolean isPalindrome(String str) {
+		int left = 0;
+		int right = str.length() - 1;
+		
+		while(left < right) {
+			if(str.charAt(left) != str.charAt(right)){
+				return false;
+			}
+			
+			left++;
+			right--;
+		}
+		
+		return true;
+	}
+
+}
---
+++ src/lecture6/string.java
@@ -0,0 +1,21 @@
+package lecture6;
+import java.util.*;
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = Integer.parseInt(scn.nextLine());
+		String str = scn.nextLine();
+		
+		System.out.println(n);
+		System.out.println(str);
+		
+//        String s1 = "sadaf";
+//        String s2 = "sadaf";
+//        s1 += 'e';
+//        System.out.println(s1);
+//        System.out.println(s2);
+	}
+
+}
