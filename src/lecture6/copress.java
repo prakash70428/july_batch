977f7544442797eac25f19eeba04c32ba4ed4c9b
prakash70428 <prakashmendish@gmail.com>
2024-08-17 23:42:31 +0530

    lecture 6 updated

---
+++ src/lecture6/copress.java
@@ -0,0 +1,51 @@
+package lecture6;
+import java.util.*;
+public class copress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        
+        System.out.println(compress1(str));
+        System.out.println(compress2(str));
+	}
+	
+	public static String compress1(String str) {
+		String ans = "";
+        ans += str.charAt(0);
+        
+        for(int i=1 ; i < str.length();i++) {
+        	if(str.charAt(i) != ans.charAt(ans.length() - 1)) {
+        		ans += str.charAt(i);
+        	}
+        }
+		
+		return ans;
+	}
+	
+	public static String compress2(String str) {
+		String ans = "";
+		ans += str.charAt(0);
+		int count = 1;
+		
+		for(int i=1 ; i < str.length();i++) {
+			if(str.charAt(i) == ans.charAt(ans.length() - 1)) {
+				count++;
+			}else {
+				if(count > 1) {
+					ans += count;	
+				}
+				ans += str.charAt(i);
+				count = 1; //reset
+			}
+		}
+		
+		if(count > 1) {
+			ans += count;	
+		}
+		
+		return ans;
+	}
+
+}
---
+++ src/lecture6/stringbuilder.java
@@ -0,0 +1,30 @@
+package lecture6;
+import java.util.*;
+public class stringbuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//        String str = "hello";
+//        
+//        long start = System.currentTimeMillis();
+//        for(int i=1;i <= 1000000;i++) {
+//        	str += 'e';
+//        }
+//        long end = System.currentTimeMillis();
+//        
+//        long duration = end - start;
+//        System.out.println(duration);
+        
+        StringBuilder sb = new StringBuilder("hello");
+        
+        long start = System.currentTimeMillis();
+        for(int i=1;i <= 1000000;i++) {
+        	sb.append('e');
+        }
+        long end = System.currentTimeMillis();
+        
+        long duration = end - start;
+        System.out.println(duration);
+	}
+
+}
