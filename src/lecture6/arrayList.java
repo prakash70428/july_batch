53f26265eb6ebb05ce87cbe138e8e6432297d88f
prakash70428 <prakashmendish@gmail.com>
2024-08-20 23:07:34 +0530

    lecture 6 updated

---
+++ src/lecture6/arrayList.java
@@ -0,0 +1,30 @@
+package lecture6;
+import java.util.*;
+public class arrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        
+        System.out.println(al);
+        System.out.println(al.size());
+        System.out.println(al.get(2));
+        
+        al.set(2,500);
+        System.out.println(al);
+        
+        al.add(2,1000);
+        System.out.println(al);
+        
+        al.remove(3);
+        System.out.println(al);
+        
+        
+	}
+
+}
