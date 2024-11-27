bf686f6f56d5985ef1d9299fc7686bb14d2d55cb
prakash70428 <prakashmendish@gmail.com>
2024-11-27 20:56:01 +0530

    lecture 14 updated

--- src/lecture14/binary_tree.java
+++ src/lecture14/binary_tree.java
@@ -47,8 +47,63 @@ public class binary_tree {
     	return root;
     }
     
-    public static void display(Node root) {
+    public static void display(Node node) {
+    	if(node == null) {
+    		return;
+    	}
     	
+    	String str = "";
+    	str += node.left == null ? "." : node.left.data + " ";
+    	str += "<- " + node.data + "-> ";
+    	str += node.right == null ? "." : node.right.data + " ";
+    	System.out.println(str); 
+    	
+    	display(node.left);
+    	display(node.right);
+    }
+    
+    public static int size(Node node) {
+    	if(node == null) {
+    		return 0;
+    	}
+    	
+    	int lans = size(node.left);
+    	int rans = size(node.right);
+    	int ans = lans + rans + 1;
+    	return ans;
+    }
+    
+    public static int sum(Node node) {
+    	if(node == null) {
+    		return 0;
+    	}
+    	
+    	int lans = sum(node.left);
+    	int rans = sum(node.right);
+    	int ans = lans + rans + node.data;
+    	return ans;
+    }
+    
+    public static int max(Node node) {
+    	if(node == null) {
+    		return Integer.MIN_VALUE;
+    	}
+    	
+    	int lans = max(node.left);
+    	int rans = max(node.right);
+    	int ans = Math.max(node.data,Math.max(lans, rans));
+    	return ans;
+    }
+    
+    public static int height(Node node) {
+    	if(node == null) {
+    		return -1;
+    	}
+    	
+    	int lh = height(node.left);
+    	int rh = height(node.right);
+    	int ans = Math.max(lh, rh) + 1;
+    	return ans;
     }
 	
 	public static void main(String[] args) {
@@ -58,6 +113,8 @@ public class binary_tree {
         
         Node root = Construct(arr);
         display(root);
+        System.out.println(size(root));
+        System.out.println(height(root));
 	}
 
 }
